63f5ee9b439d80950cc00222f148a2503cec5e5e
Jaber <rantisijaber@gmail.com>
2025-02-28 17:32:11 -0500

    Created User model and built corresponding database table "user"

---
+++ src/main/java/com/jaberrantisi/contactmanager/model/User.java
@@ -0,0 +1,39 @@
+package com.jaberrantisi.contactmanager.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "user")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(updatable = false, nullable = false, name = "id")
+    private UUID userId;
+
+    @Column(nullable = false, name = "first_name")
+    private String firstName;
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+}
