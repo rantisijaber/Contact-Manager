110c7dc3b0d532ce2500e96706a5b1059d4f0b6c
Jaber <rantisijaber@gmail.com>
2025-03-04 21:47:19 -0500

    Fixed typing of id

--- src/main/java/com/jaberrantisi/contactmanager/model/User.java
+++ src/main/java/com/jaberrantisi/contactmanager/model/User.java
@@ -2,16 +2,14 @@ package com.jaberrantisi.contactmanager.model;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.UUID;
 
 
 @Entity
 @Table(name = "user")
+@Setter
 @Getter
 @Builder
 @NoArgsConstructor
