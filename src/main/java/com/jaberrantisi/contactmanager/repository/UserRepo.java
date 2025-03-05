2ef4328a89da9689c5584340948f132bef65103f
Jaber <rantisijaber@gmail.com>
2025-03-04 21:40:16 -0500

    Added a find by username method

--- src/main/java/com/jaberrantisi/contactmanager/repository/UserRepo.java
+++ src/main/java/com/jaberrantisi/contactmanager/repository/UserRepo.java
@@ -2,10 +2,13 @@ package com.jaberrantisi.contactmanager.repository;
 
 import com.jaberrantisi.contactmanager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-public interface UserRepo extends JpaRepository<User, UUID> {
 
+@Repository
+public interface UserRepo extends JpaRepository<User, UUID> {
+    User findByUsername(String username);
 
 }
