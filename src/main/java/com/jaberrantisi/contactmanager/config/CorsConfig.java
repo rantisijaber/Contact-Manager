480d966125ca2365b18fee7f42d82c78b494c89e
Jaber <rantisijaber@gmail.com>
2025-03-26 09:46:26 -0400

    Configured CORS to allow frontend to use backend REST APIs

---
+++ src/main/java/com/jaberrantisi/contactmanager/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.jaberrantisi.contactmanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3002")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
