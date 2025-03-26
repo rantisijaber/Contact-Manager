c8d62c96b9cfaf15e1c4d235ff006a19a2a3642a
Jaber <rantisijaber@gmail.com>
2025-03-26 09:47:11 -0400

    Added JWT filter and worked out its logic to allow for JWT Authentication

--- src/main/java/com/jaberrantisi/contactmanager/service/JWTService.java
+++ src/main/java/com/jaberrantisi/contactmanager/service/JWTService.java
@@ -1,9 +1,11 @@
 package com.jaberrantisi.contactmanager.service;
 
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
@@ -11,10 +13,11 @@ import javax.crypto.SecretKey;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
+import java.util.function.Function;
 
 @Service
 public class JWTService {
-    private String sk;
+    private final String sk;
 
     public JWTService() {
         try {
@@ -35,17 +38,41 @@ public class JWTService {
                 .add(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 30 ))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 30 ))
                 .and()
                 .signWith(getKey())
                 .compact();
 
 
     }
-    private Key getKey() {
+    private SecretKey getKey() {
         byte[] keyBytes = Decoders.BASE64.decode(sk);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
 
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
 }
