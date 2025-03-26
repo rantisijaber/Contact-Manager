74bb280faa91e04a6619bd4409b57290f3c46a12
Jaber <rantisijaber@gmail.com>
2025-03-26 09:51:08 -0400

    Fixed exception

--- src/main/java/com/jaberrantisi/contactmanager/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/jaberrantisi/contactmanager/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.jaberrantisi.contactmanager.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException() {
-        super("Resource not found");
+    public ResourceNotFoundException(String message) {
+
     }
 }
